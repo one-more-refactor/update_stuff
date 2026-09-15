e395007a922933b310c9ec10e6f3d2b9651a02f0
Philip Ludwig <297687349+one-more-refactor@users.noreply.github.com>
2026-09-15 11:41:25 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -129,7 +129,8 @@ tmux new-session -d -s "$tmux_session_name" "$payload $show_output && exit"
 
 #Attach to tmux session
 check_awnser
-if [ $awnser == "0" ]; then
+
+if [ ! $awnser == "" ]; then 
     if [ check_tmux_session = 0 ]; then
             tmux attach-session -t $tmux_session_name
     else
